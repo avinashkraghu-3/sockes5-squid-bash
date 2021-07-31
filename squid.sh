e8a22ab156909977cce91d41d87dbd8e6bee0e8e
avi_downloads <77006304+avinashkraghu-3@users.noreply.github.com>
2021-07-31 16:37:58 +0530

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+function Installation(){
+
 if [ `whoami` != root ]; then
 	echo "ERROR: You need to run the script as user root or add sudo before command."
 	exit 1
@@ -14,8 +16,12 @@ if [[ -d /etc/squid/ || -d /etc/squid3/ ]]; then
 fi
 
 #squid variable
-squid_user=admin
-squid_password=firefox@123
+read -r -p "squid_user(default username:admin):" -i "admin" squid_user
+read -r -p "squid_password(default password:qwerty@123)" -i "qwerty@123" squid_password
+echo $squid_user\n$squid_password > /etc/squid/squid_login
+
+#squid_user=admin 
+#squid_password=firefox@123
 
 #install required packages
 /usr/bin/apt -y install apache2-utils squid3 wget
@@ -36,7 +42,7 @@ sudo mv /etc/squid/squid-1.conf /etc/squid/squid.conf
 sudo chown proxy /etc/squid/squid_passwd
 
 #htpassword set
-/usr/bin/htpasswd -b -c /etc/squid/passwd $squid_user $squid_password
+/usr/bin/htpasswd -b -c /etc/squid/squid_passwd $squid_user $squid_password
 
 #iptable port allow rule
 /sbin/iptables -I INPUT -p tcp --dport 17361 -j ACCEPT
@@ -44,3 +50,32 @@ sudo chown proxy /etc/squid/squid_passwd
 
 #service restart
 service squid restart
+}
+
+
+function Uninstallation(){
+ echo -e '[*] Uninstalling SQUID Server'
+ apt-get remove --purge squid &> /dev/null
+ rm -rf /etc/squid*
+ echo -e '[√] Squid3 Server successfully uninstalled and removed.'
+}
+
+
+clear
+echo -e " Choose squid Proxy Installtion"
+echo -e " [1] Install Squid"
+echo -e " [2] Uninstall Squid"
+until [[ "$opts" =~ ^[1-2]$ ]]; do
+	read -rp " Choose from [1-2]: " -e opts
+	done
+	case $opts in
+
+	1)
+	Installation
+	;;	
+	2)
+	Uninstallation
+	exit 1
+	;;
+esac
+exit 1
